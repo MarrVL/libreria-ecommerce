6b6ecc5931f0924adf8073dcf8e8e1b49e116bea
MiguelAngelJimenezUria <miguel.jimenez@tecsup.edu.pe>
2025-06-28 12:48:27 -0500

    usuarios y cuenta ya funcionales

--- libreria/libreria/settings.py
+++ libreria/libreria/settings.py
@@ -74,7 +74,7 @@ ROOT_URLCONF = 'libreria.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates', BASE_DIR / 'theme' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
