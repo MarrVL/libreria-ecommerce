c338aa483031b1f718756e04e85159e6998eccc8
ValeriaZP135 <valeria.zumaran@tecsup.edu.pe>
2025-10-26 20:14:38 -0500

    GoogleCloud Database
    Migración a base de datos de Google Cloud

--- libreria/libreria/settings.py
+++ libreria/libreria/settings.py
@@ -107,8 +107,12 @@ WSGI_APPLICATION = 'libreria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': os.environ.get('DB_NAME', 'lecturama'),
+        'USER': os.environ.get('DB_USER', 'lecturama-user'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', '$jiEZ3YY1~<)%R_B'),
+        'HOST': os.environ.get('DB_HOST', '34.176.98.98'), 
+        'PORT': os.environ.get('DB_PORT', '5432'), 
     }
 }
 
